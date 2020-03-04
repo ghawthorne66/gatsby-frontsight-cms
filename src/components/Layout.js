27fe8d369aa5f85367a4d6bc1c808aee25d3d88f
Greg Hawthorne <ghawthorne@me.com>
2020-03-04 13:04:20 -0800

    add icon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,20 +16,20 @@ const TemplateWrapper = ({ children }) => {
         <meta name="description" content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          rel="fs_icon"
+          sizes="192x192"
+          href={`${withPrefix('/')}img/favicon_192x192.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${withPrefix('/')}img/favicon_32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${withPrefix('/')}img/favicon_16x16.png`}
           sizes="16x16"
         />
 
